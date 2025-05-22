c66651566664cece349fbd223874111f84d93ded
order_yo <markovmaks895@gmail.com>
2025-05-22 17:57:51 +0300

    Обновилена факторка БД: - новый JDBC драйвер - внедрён конфиг для БД

---
+++ data/src/main/kotlin/db/DatabaseConfig.kt
@@ -0,0 +1,6 @@
+package db
+
+data class DatabaseConfig(
+    val url: String,
+    val driver: String
+)
--- data/src/main/kotlin/db/DatabaseFactory.kt
+++ data/src/main/kotlin/db/DatabaseFactory.kt
@@ -1,5 +1,6 @@
 package db
 
+import com.zaxxer.hikari.HikariDataSource
 import db.tables.FilesMetadata
 import db.tables.Users
 import db.tables.Spaces
@@ -7,19 +8,52 @@ import db.tables.SpacesUsers
 import db.tables.ApiKeys
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
-    fun init() {
-        Database.connect("jdbc:sqlite:data.db", driver = "org.sqlite.JDBC")
+
+    private lateinit var dataSource: HikariDataSource
+
+    fun init(config: DatabaseConfig) {
+        val cpConfig = HikariDataSource().apply {
+            jdbcUrl = config.url
+            driverClassName = config.driver
+            maximumPoolSize = 5
+            isAutoCommit = false
+        }
+        dataSource = HikariDataSource(cpConfig)
+        Database.connect(dataSource)
+    }
+
+    fun createSchemaIfNotExists() {
         transaction {
-            SchemaUtils.create(
-                Users,
-                ApiKeys,
-                Spaces,
-                FilesMetadata,
-                SpacesUsers
-            )
+            if (!tableExists()) {
+                transaction {
+                    SchemaUtils.create(
+                        Users,
+                        ApiKeys,
+                        Spaces,
+                        FilesMetadata,
+                        SpacesUsers
+                    )
+                }
+            }
         }
     }
+
+    private fun tableExists(): Boolean {
+        return try {
+            TransactionManager.current().exec("SELECT name FROM sqlite_master WHERE type='table' AND name='Users'") {
+                it.next()
+            } ?: false
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun close() {
+        dataSource.close()
+    }
+
 }
